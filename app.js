8ed3d3f68d87dd62355f3f974f610a9c901edfd6
Julian Johannesen <julian.johannesen@gmail.com>
2019-12-16 12:47:08 -0500

    initial

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const favicon = require("serve-favicon");
 const path = require("path");
+const createError = require("http-errors");
 const logger = require("morgan");
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
@@ -8,7 +9,7 @@ const multer = require("multer");
 const { body, validationResult } = require("express-validator");
 
 const indexRouter = require("./routes/index");
-const usersRouter = require("./routes/users");
+const profileRouter = require("./routes/profile");
 
 const upload = multer();
 const app = express();
@@ -33,7 +34,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Use these routes
 app.use("/", indexRouter);
-app.use("/users", usersRouter);
+app.use("/profile", profileRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -50,40 +51,5 @@ app.use(function(err, req, res, next) {
 	res.status(err.status || 500);
 	res.render("error");
 });
-
-
-/*
-ROUTES
-*/
-
-// app.get('/', function(req,res){
-    //     res.render('form');
-    // });
-    
-    // app.post(
-        //     // The route
-        //     '/',
-        //     // Validation middleware
-        //     [
-            //     // username must be plain text
-            //     body('username').not().isEmpty().trim().isAlphanumeric().isLength({min: 2, max: 25})
-            //     ],
-            //     // Request/response handler
-            //     (req, res) => {
-                //     // Finds the validation errors in this request and wraps them in an object with handy functions
-                //     const errors = validationResult(req);
-                //     if (!errors.isEmpty()) {
-                    //         return res.status(422).json({ errors: errors.array() });
-//     }
-
-//     console.log(req.body);
-//     res.send(`${req.body.username} was found.`)
-// });
-
-/*
-PORT
-*/
-// let port = process.env.PORT || 3000
-// app.listen(port);
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,24 @@
-const express = require("express");
+const express = require('express');
+//const cookie = require("cookie-parser");
 const router = express.Router();
 
 /* GET home page. */
-router.get("/", function(req, res, next) {
-	res.render("form", {"title":"Creepin' on Colleagues"});
+router.get('/', function(req, res, next) {
+    res.render('form', {title: "Creepin' on Colleagues"});
 });
+
+router.post(
+    // The route
+	'/',
+	// This is where validation middle ware should appear
+	// Handle request and response
+    (req, res) => {
+		// This where error checking should appear
+		// Set a cookie. It seems that you can do this without any middlewear.
+		res.cookie("username", req.body.username);
+		// Then use the cookie-parser middlewear to read the value of the cookie. In index.js I required cookie-parser and used a app.use() to register it
+		res.send(`You're interested in ${req.cookies.username ? req.cookies.username : req.body.username}`);
+    }
+);
 
 module.exports = router;
