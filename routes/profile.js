e17d5b1ccc5f30c5bebf806bde72c1aa4dbe733d
github username <github email address>
2020-01-07 19:13:38 -0500

    fixed a route bug

--- routes/profile.js
+++ routes/profile.js
@@ -2,7 +2,7 @@ var express = require("express");
 var router = express.Router();
 
 /* GET profile page. */
-router.get("/profile", function(req, res, next) {
+router.get("/", function(req, res, next) {
 	res.render("profile");
 });
 
